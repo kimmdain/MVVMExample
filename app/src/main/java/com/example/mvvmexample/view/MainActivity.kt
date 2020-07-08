f7c705041875dc364dfe82fb3ea90c5dca53dd9d
kimmdain <97dain@gmail.com>
2020-07-08 15:38:45 +0900

    fix: Koin err(by viewModel()), layout(ViewDataBinding) err

--- app/src/main/java/com/example/mvvmexample/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmexample/view/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.mvvmexample.view
 import android.os.Bundle
 import com.example.mvvmexample.R
 import com.example.mvvmexample._base.BaseKotlinActivity
+import com.example.mvvmexample.databinding.ActivityMainBinding
 import com.example.mvvmexample.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : BaseKotlinActivity< MainViewModel>() {
+class MainActivity : BaseKotlinActivity<ActivityMainBinding, MainViewModel>() {
 
     override val layoutResourceId: Int
-    get() = R.layout.activity_main
+        get() = R.layout.activity_main
     override val viewModel: MainViewModel by viewModel()
 
     override fun initStartView() {
@@ -25,8 +27,8 @@ class MainActivity : BaseKotlinActivity< MainViewModel>() {
     }
 
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
--- app/src/main/java/com/example/mvvmexample/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmexample/viewmodel/MainViewModel.kt
@@ -4,6 +4,8 @@ import com.example.mvvmexample._base.BaseKotlinActivity
 import com.example.mvvmexample._base.BaseKotlinViewModel
 import com.example.mvvmexample.model.DataModel
 
+
 class MainViewModel(private val model: DataModel): BaseKotlinViewModel() {
+
 
 }
